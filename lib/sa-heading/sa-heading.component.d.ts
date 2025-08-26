1cad6c090d6c35c74f6e52c3bd6235ad7d821fcd
Alvaro Mena Santos <alvaro.mena@encora.com>
2025-08-26 18:48:59 -0500

    build: sanna-ui v0.0.103

--- lib/sa-heading/sa-heading.component.d.ts
+++ lib/sa-heading/sa-heading.component.d.ts
@@ -3,7 +3,7 @@ type HeadingSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5
 type HeadingWeight = 'bold' | 'regular' | 'light' | 'semibold';
 type HeadingMargin = '0' | '1' | '2' | '3' | '4' | '5' | 'auto';
 export declare class SaHeadingComponent {
-    children: string;
+    text: string;
     private _size;
     private _weight;
     private _mt?;
@@ -24,6 +24,6 @@ export declare class SaHeadingComponent {
     get ml(): HeadingMargin | undefined;
     get headingClasses(): string;
     static ɵfac: i0.ɵɵFactoryDeclaration<SaHeadingComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<SaHeadingComponent, "sa-heading", never, { "children": { "alias": "children"; "required": false; }; "size": { "alias": "size"; "required": false; }; "weight": { "alias": "weight"; "required": false; }; "mt": { "alias": "mt"; "required": false; }; "mb": { "alias": "mb"; "required": false; }; "mr": { "alias": "mr"; "required": false; }; "ml": { "alias": "ml"; "required": false; }; }, {}, never, never, false, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<SaHeadingComponent, "sa-heading", never, { "text": { "alias": "text"; "required": false; }; "size": { "alias": "size"; "required": false; }; "weight": { "alias": "weight"; "required": false; }; "mt": { "alias": "mt"; "required": false; }; "mb": { "alias": "mb"; "required": false; }; "mr": { "alias": "mr"; "required": false; }; "ml": { "alias": "ml"; "required": false; }; }, {}, never, never, false, never>;
 }
 export {};
