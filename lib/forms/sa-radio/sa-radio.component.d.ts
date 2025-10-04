cc6d20293e06e78712d8fcfa1178c855458d63bc
Alvaro Mena Santos <alvaro.mena@encora.com>
2025-10-03 23:25:50 -0500

    build: sanna-ui v0.0.136

--- lib/forms/sa-radio/sa-radio.component.d.ts
+++ lib/forms/sa-radio/sa-radio.component.d.ts
@@ -26,8 +26,8 @@ export declare class SaRadioComponent implements ControlValueAccessor, OnInit, O
     class: string;
     valueChange: EventEmitter<any>;
     change: EventEmitter<Event>;
-    focus: EventEmitter<FocusEvent>;
-    blur: EventEmitter<FocusEvent>;
+    focusin: EventEmitter<FocusEvent>;
+    focusout: EventEmitter<FocusEvent>;
     selectedValue: any;
     isFocused: boolean;
     private _generatedId;
@@ -48,9 +48,9 @@ export declare class SaRadioComponent implements ControlValueAccessor, OnInit, O
     registerOnChange(fn: any): void;
     registerOnTouched(fn: any): void;
     setDisabledState(isDisabled: boolean): void;
-    onRadioChange(event: Event): void;
+    onRadioClick(event: Event): void;
     onRadioFocus(event: FocusEvent): void;
     onRadioBlur(event: FocusEvent): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<SaRadioComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<SaRadioComponent, "sa-radio", never, { "value": { "alias": "value"; "required": false; }; "size": { "alias": "size"; "required": false; }; "status": { "alias": "status"; "required": false; }; "label": { "alias": "label"; "required": false; }; "noLabel": { "alias": "noLabel"; "required": false; }; "hideLabel": { "alias": "hideLabel"; "required": false; }; "helperText": { "alias": "helperText"; "required": false; }; "errorText": { "alias": "errorText"; "required": false; }; "required": { "alias": "required"; "required": false; }; "disabled": { "alias": "disabled"; "required": false; }; "readonly": { "alias": "readonly"; "required": false; }; "id": { "alias": "id"; "required": false; }; "name": { "alias": "name"; "required": false; }; "class": { "alias": "class"; "required": false; }; }, { "valueChange": "valueChange"; "change": "change"; "focus": "focus"; "blur": "blur"; }, never, never, false, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<SaRadioComponent, "sa-radio", never, { "value": { "alias": "value"; "required": false; }; "size": { "alias": "size"; "required": false; }; "status": { "alias": "status"; "required": false; }; "label": { "alias": "label"; "required": false; }; "noLabel": { "alias": "noLabel"; "required": false; }; "hideLabel": { "alias": "hideLabel"; "required": false; }; "helperText": { "alias": "helperText"; "required": false; }; "errorText": { "alias": "errorText"; "required": false; }; "required": { "alias": "required"; "required": false; }; "disabled": { "alias": "disabled"; "required": false; }; "readonly": { "alias": "readonly"; "required": false; }; "id": { "alias": "id"; "required": false; }; "name": { "alias": "name"; "required": false; }; "class": { "alias": "class"; "required": false; }; }, { "valueChange": "valueChange"; "change": "change"; "focusin": "focusin"; "focusout": "focusout"; }, never, never, false, never>;
 }
